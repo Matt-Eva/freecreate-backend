995bbda4908e644b0c015170879583d520e46763
Matt Eva <matteweva@gmail.com>
2023-01-07 10:52:52 -0800

    using router.route()

--- routes.js
+++ routes.js
@@ -3,21 +3,22 @@ const router = express.Router()
 
 const db = require ('./conn.js')
 
-router.get( "/listings", async function (_req, res){
-    // res.send("success!")
-    const dbConn = db.getDbConn()
-    try {
-        console.log("fetching listings")
-        const listings = await dbConn
-                                    .collection('listingsAndReviews')
-                                    .find({})
-                                    .limit(50)
-                                    .toArray()
-        res.status(200).json(listings)
-    } catch (error){
-        console.error(error)
-        res.status(400)
-    }
-})
+router.route("/listings")
+    .get(async function (_req, res){
+        // res.send("success!")
+        const dbConn = db.getDbConn()
+        try {
+            console.log("fetching listings")
+            const listings = await dbConn
+                                        .collection('listingsAndReviews')
+                                        .find({})
+                                        .limit(50)
+                                        .toArray()
+            res.status(200).json(listings)
+        } catch (error){
+            console.error(error)
+            res.status(400)
+        }
+    })
 
 module.exports = router;
