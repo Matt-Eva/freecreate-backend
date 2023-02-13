4743a9c784e03a1c1be24cc4836803993830742e
Matt Eva <matteweva@gmail.com>
2023-02-12 16:41:10 -0800

    bcrypt-installed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,20 +1,10 @@
 const db = require('../db/conn.js')
-
-exports.index = async function(req, res){
-    const dbConn = db.getDbConn()
-    try {
-        console.log("fetching users")
-        const users = await dbConn.collection('users').find({}).limit(50).toArray()
-        res.status(200).json(users)
-    } catch (error){
-        console.error(error)
-    }
-}
+const bcrypt = require('bcrypt')
 
 exports.login = function (req, res){
     console.log('hit')
     req.session.userId = 1
-    res.status(200).send('Logged In')
+    res.status(200).send({message: 'Logged In'})
 }
 
 exports.authorize = function (req, res){
@@ -23,5 +13,21 @@ exports.authorize = function (req, res){
         res.status(200).send("Authorized")
     } else {
         res.status(401).send("Unauthorized")
+    }
+}
+
+exports.logout = function (req, res){
+    console.log(req.session.userId)
+    req.session = null
+}
+
+exports.create = async function(req, res){
+    console.log("creating user")
+    const dbConn = db.getDbConn()
+    const data = req.body
+    try {
+        
+    } catch (error){
+        res.status(422).send({error: "Could not process data"})
     }
 }
