e73d37a7f8d122d4b4194541c84ed14c4c078e02
Matt Eva <matteweva@gmail.com>
2023-03-07 17:31:56 -0800

    added conditional logic

--- controllers/storiesController.js
+++ controllers/storiesController.js
@@ -11,7 +11,32 @@ exports.index = async (req, res)=>{
     }
 }
 
-exports.search = async(req, res) =>{
+exports.search = async (req, res) =>{
+    const tags = req.body.tags
+    const date = req.body.date
+    const genre = req.body.genre
+    const currentDate = Date.now()
+    const dateObj = {
+        "Most Recent": null,
+        "Past Day": currentDate - 8.64 * 10**7,
+        "Past Week": currentDate - 6.05 * 10**8,
+        "Past Month": currentDate - 2.62 * 10**9,
+        "Past Year": currentDate - 3.14 * 10**10,
+    }
+    const dateRange = dateObj[date]
+
+    console.log(dateRange)
+
+    if (tags.length === 0 && dateRange){
+        console.log("no tag but date")
+    } else if(tags.length === 0){
+        console.log("no tag no date")
+    } else if (dateRange){
+        console.log("tag and date")
+    } else {
+        console.log(" tag but no date")
+    }
+
     console.log(req.body)
     res.send({message: "received"})
 }
