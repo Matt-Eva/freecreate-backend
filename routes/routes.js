8abc7a3b143f587c602961bb5d1c31e41b7b7c02
Matt Eva <matteweva@gmail.com>
2023-03-07 17:36:02 -0800

    set up helper function folder for controllers

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const usersController = require("../controllers/usersController")
-const shortStoriesController = require("../controllers/storiesController")
+const shortStoriesController = require("../controllers/shortStoriesController")
 
 
 // === USER ROUTES ===
